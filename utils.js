25f9ce778919cd8fd1be8f6492586816e0848f69
Devendra Lodhi <anshul.lodhi.game@gmail.com>
2025-10-03 18:35:17 +0530

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,24 @@
+export function parseCSV(csvText) {
+  const lines = csvText.split('\n').filter(Boolean);
+  const result = lines.map(line => {
+    const [name, website] = line.split(',');
+    return { name: name.trim(), website: website.trim() };
+  });
+  return result;
+}
+
+export function downloadCSV(data, filename) {
+  const headers = Object.keys(data[0]).join(',');
+  const csv = [
+    headers,
+    ...data.map(row => Object.values(row).join(','))
+  ].join('\n');
+
+  const blob = new Blob([csv], { type: 'text/csv' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(url);
+}
